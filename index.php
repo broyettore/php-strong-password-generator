25e021a4f59c760e2e70c02ac21937431dac08e8
broyettore <broyettore@gmail.com>
2023-04-11 20:55:06 +0200

    milestone 2 done

--- index.php
+++ index.php
@@ -1,22 +1,6 @@
 <?php
 
-$passChars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-$genPass = $_GET["genPass"];
-
-function createPassword($passlen, $passCombo) {
-
-    $password = "";
-
-    if (!empty($passlen)) {
-        for ($i = 0; $i <= $passlen - 1; $i++) {
-            $randomNumber = random_int(0, strlen($passCombo) - 1);
-            $randomPass= $passCombo[$randomNumber];
-            $password .= $randomPass;
-           }
-    }
-
-    return $password;
-};
+include ("functions.php");
 
 ?>
 
